39819786ab02c06a8e9eb8ef0eff1b7120121c31
Tim <tim.esselens@gmail.com>
2011-05-10 14:21:10 +0200

    FEAT: state in url, bookmarks (not saved)

--- html/js/script.js
+++ html/js/script.js
@@ -12,6 +12,8 @@ $(function() {
         }
     });
 
+    window.spincount = 0; // internal spincount locker
+
     $('div.ui-layout-container').layout({
         north: {
             size: 250
@@ -24,6 +26,12 @@ $(function() {
     } else {
         // alert('no localstorage support, upgrade your browser');
     }
+
+    var urlQuerySearch = window.location.toString().match(/\/s\/([^\/]*)/);
+    var urlQueryDocUID = window.location.toString().match(/\/d\/([\w\d\-\/]+)/);
+
+    if(urlQuerySearch) { $('input[name=q]').val(urlQuerySearch[1]); submit(); }
+    if(urlQueryDocUID) { show(urlQueryDocUID[1]); }
                 
 
     // helper functions -------------------------------------------------------------------------------------------------------
@@ -57,9 +65,11 @@ $(function() {
         ].join(''); 
     }
 
+
+
     function submit() {
         var query = $('input[name=q]').val();
-        $('img.spinner').show();
+        $('img.spinner').show(); window.spincount++;
         $.ajax({
             url: "/api/list.json", 
             data: { q: query },
@@ -68,11 +78,38 @@ $(function() {
                 var h = [];
                 for (var key in json) { h.push(render(json[key],query)); }
                 $("#result").html(h.join(''));
-                $('img.spinner').hide();
+                if(urlQueryDocUID) { 
+                    var docuid = urlQueryDocUID[1];
+                    $("#result tr.selected").removeClass('selected');
+                    console.log('docuid %o el %o', docuid, $("tr.entry[rel='"+docuid+"']"));
+                    $("#result tr.entry[rel='"+docuid+"']").addClass('selected');
+                }
+                if(--window.spincount == 0) { $('img.spinner').hide(); }
             },
             error: function() {
                 alert('unable to fetch data');
-                $('img.spinner').hide();
+                if(--window.spincount == 0) { $('img.spinner').hide(); }
+            }
+        });
+    }
+
+    function show(docuid) {
+        var query = $('input[name=q]').val();
+        $('img.spinner').show(); window.spincount++;
+        $.ajax({
+            url: "/doc/show.html",
+            data: { id: docuid },
+            dataType: 'html',
+            success: function(h) {
+                if(query) { h = h.replace(query,'<em class="hl">'+query+'</em>'); }
+                $(".mainview").empty().html(h);
+                $("#result tr.selected").removeClass('selected');
+                $("#result tr.entry[rel='"+docuid+"']").addClass('selected');
+                window.history.pushState({},'BeLawsHub', '/s/' + query + '/d/' + docuid);
+                if(--window.spincount == 0) { $('img.spinner').hide(); }
+            },
+            error: function() {
+                if(--window.spincount == 0) { $('img.spinner').hide(); }
             }
         });
     }
@@ -81,41 +118,41 @@ $(function() {
     $("#result tr.entry").live('click',function(e) {
         // console.log('fooooo %o',$(this).attr('rel'));
         var docuid = $(this).attr('rel');
-        var query = $('input[name=q]').val();
-        $('img.spinner').show();
-        $.ajax({
-            url: "/doc/show.html",
-            data: { id: docuid },
-            dataType: 'html',
-            success: function(h) {
-                if(query) { h = h.replace(query,'<em class="hl">'+query+'</em>'); }
-                $(".mainview").empty().html(h);
-                $('img.spinner').hide();
-            },
-            error: function() {
-                $('img.spinner').hide();
-            }
-        });
+        show(docuid);
+    });
+
+    $('ul.panellist img#add_bookmark').bind('click',function(e) {
+        var urlQuerySearch = window.location.toString().match(/\/s\/([^\/]+)/);
+        var urlQueryDocUID = window.location.toString().match(/\/d\/([\w\d\-\/]+)/);
+
+        $('ul.panellist ul.bookmarklist').append(['<li><a href="',window.location.pathname,'"><span class="docuid">',urlQueryDocUID[1],'</span><span class="search">',urlQuerySearch[1],'</span></a></li>'].join(''));
     });
 
     $("ul.panellist ul.historylist li").live('click', function(e) {
         $('input[name=q]').val($(this).text().trim());
+        var query = $('input[name=q]').val();
+        window.history.pushState({},'BeLawsHub', '/s/' + query );
         submit();
+        $(".mainview").empty();
     });
 
     $("input[type=submit]").bind('click', function(e) {
         e.preventDefault();
         submit();
+        $(".mainview").empty();
         var query = $('input[name=q]').val();
         $('ul.panellist ul.historylist').append(['<li>',query,'</li>'].join(''));
+        window.history.pushState({},'BeLawsHub', '/s/' + query );
         return false;
     });
 
     $("form").bind("submit",function(e) {
         e.preventDefault();
         submit();
+        $(".mainview").empty();
         var query = $('input[name=q]').val();
         $('ul.panellist ul.historylist').append(['<li>',query,'</li>'].join(''));
+        window.history.pushState({},'BeLawsHub', '/s/' + query );
         return false;
     });
 });
